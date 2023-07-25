552cd09db8f194359387f8286a232706c8a83512
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-25 19:02:56 +0200

    finito send message e automatic answer

--- script.js
+++ script.js
@@ -127,6 +127,85 @@ createApp({
                         }
                     ],
                 },
+                {
+                    name: "Michele",
+                    avatar: "_5",
+                    messages: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            message: "Hai portato a spasso il cane?",
+                            status: "sent",
+                        },
+                        {
+                            date: "10/01/2020 15:50:00",
+                            message: "Ricordati di dargli da mangiare",
+                            status: "sent",
+                        },
+                        {
+                            date: "10/01/2020 16:15:22",
+                            message: "Tutto fatto!",
+                            status: "received",
+                        },
+                    ],
+                },
+                {
+                    name: "Sara",
+                    avatar: "_6",
+                    messages: [
+                        {
+                            date: "20/03/2020 16:30:00",
+                            message: "Ciao come stai?",
+                            status: "sent",
+                        },
+                        {
+                            date: "20/03/2020 16:30:55",
+                            message: "Bene grazie! Stasera ci vediamo?",
+                            status: "received",
+                        },
+                        {
+                            date: "20/03/2020 16:35:00",
+                            message: "Certo! Vedrai che ci divertiremo.",
+                            status: "sent",
+                        },
+                    ],
+                },
+                {
+                    name: "Samuele",
+                    avatar: "_7",
+                    messages: [
+                        {
+                            date: "28/03/2020 10:10:40",
+                            message: "La Marianna va in campagna",
+                            status: "received",
+                        },
+                        {
+                            date: "28/03/2020 10:20:10",
+                            message: "Sicuro di non aver sbagliato chat?",
+                            status: "sent",
+                        },
+                        {
+                            date: "28/03/2020 16:15:22",
+                            message: "Ah scusa!",
+                            status: "received",
+                        },
+                    ],
+                },
+                {
+                    name: "Davide",
+                    avatar: "_8",
+                    messages: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            message: "Lo sai che ha aperto una nuova pizzeria?",
+                            status: "sent",
+                        },
+                        {
+                            date: "10/01/2020 15:50:00",
+                            message: "Si, ma preferirei andare al cinema",
+                            status: "received",
+                        },
+                    ],
+                },
             ]
         }
     },
@@ -139,7 +218,41 @@ createApp({
         setCurrentContact(index) {
             this.currentChat = index;
         },
-    }
+        //Send message
+        sendMessage() {
+            const d = new Date();
+            let newdate = d.toDateString();
+            const newmSentMessage = {
+                date: newdate,
+                message: this.newmessage,
+                status: 'sent'
+            }
+            this.contacts[this.currentChat].messages.push(newmSentMessage);
+        },
+        //send message
+        sendMessage() {
+            const d = new Date();
+            let newdate = d.toDateString();
+            const newmSentMessage = {
+                date: newdate,
+                message: this.newmessage,
+                status: 'sent'
+
+            }
+            this.contacts[this.currentChat].messages.push(newmSentMessage);
+            //automatic answer
+            setTimeout(() => {
+                const d = new Date();
+                let newdate = d.toDateString();
+                const newmSentMessage = {
+                    date: newdate,
+                    message: 'ok',
+                    status: 'received',
+                }
+                this.contacts[this.currentChat].messages.push(newmSentMessage);
+            }, 1000);
+        },
+    },
 }).mount('#app');
 
 
